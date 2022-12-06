53442912885cb60dd472661f68389ca8145c1c82
nahuelz123 <84518078+nahuelz123@users.noreply.github.com>
2022-12-05 23:15:31 -0300

    1

--- FrameworkSessionFuncionaPerfercto/Models/Book.php
+++ FrameworkSessionFuncionaPerfercto/Models/Book.php
@@ -9,19 +9,11 @@
 
         
 
-        /**
-         * Get the value of code
-         */ 
         public function getCode()
         {
                 return $this->code;
         }
 
-        /**
-         * Set the value of code
-         *
-         * @return  self
-         */ 
         public function setCode($code)
         {
                 $this->code = $code;
@@ -29,19 +21,12 @@
                 return $this;
         }
 
-        /**
-         * Get the value of title
-         */ 
         public function getTitle()
         {
                 return $this->title;
         }
 
-        /**
-         * Set the value of title
-         *
-         * @return  self
-         */ 
+       
         public function setTitle($title)
         {
                 $this->title = $title;
@@ -49,19 +34,13 @@
                 return $this;
         }
 
-        /**
-         * Get the value of price
-         */ 
+   
         public function getPrice()
         {
                 return $this->price;
         }
 
-        /**
-         * Set the value of price
-         *
-         * @return  self
-         */ 
+  
         public function setPrice($price)
         {
                 $this->price = $price;
